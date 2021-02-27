83bd076e177cc9842c7ab51d585bbd3eb90244c7
andreyMartinez <ram.v98@hotmail.com>
2021-02-26 19:16:25 -0500

    finish my first header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,18 +6,25 @@ function Header(props) {
     return (
         <header className="header">
             <nav className="navigator">
-                <ul>
-                    <li>
-                        <a href="">Sobre mi</a>
+                <ul className="list-content">
+                    <li className="list">
+                        <a  className="link" href="">Sobre mi</a>
                     </li>
-                    <li>
-                        <a href="">Experiencia</a>
+                    <li className="list">
+                        <a className="link" href="">Experiencia</a>
                     </li>
-                    <li>
-                        <a href="">Conocimiento</a>
+                    <li className="list">
+                        <a className="link" href="">Conocimiento</a>
                     </li>
+                    <li className="list">
+                        <a className="link" href="">Contactame</a>
+                    </li>
+                </ul>
+                <ul className="list-content right">
                     <li>
-                        <a href="">Contactame</a>
+                    <a className="link" href="">
+                        <img className="img-profile" src="https://lh3.google.com/u/0/ogw/ADGmqu_BVkzxawtJymUyXdPBVR61_rjCuog0NpfBw5O7=s32-c-mo" alt="icon-raphael-martinez"/>
+                        </a>
                     </li>
                 </ul>
             </nav>
